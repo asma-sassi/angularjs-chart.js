ae92673dafc02fa7584290abe895ef8484d17c7a
Asma Sassi <asmasassi123@gmail.com>
2018-01-02 14:59:19 +0100

    add bar chart

--- app.js
+++ app.js
@@ -2,18 +2,9 @@
 // create the module named streamapp and include ngRoute for routing needs
 var streamApp = angular.module('streamApp',['ngRoute','chart.js']);
 
-streamApp.config(['$qProvider', function ($qProvider) {
-    $qProvider.errorOnUnhandledRejections(false);
-}]);
-
-// Import colors into chart
-// streamApp.config((function (ChartJsProvider) {
-//     ChartJsProvider.setOptions({
-//         colors : [ '#803690', '#00ADF9', '#DCDCDC', '#46BFBD', '#FDB45C', '#949FB1', '#4D5360'],
-//         responsive: true,
-//         beginAtZero: true
-//     });
-// }));
+// streamApp.config(['$qProvider', function ($qProvider) {
+//     $qProvider.errorOnUnhandledRejections(false);
+// }]);
 
 //configure app's routes
 streamApp.config(['$routeProvider','$locationProvider',function ($routeProvider,$locationProvider) {
--- controllers/MainController.js
+++ controllers/MainController.js
@@ -2,44 +2,45 @@
 var streamApp = angular.module('streamApp');
 streamApp.config(['ChartJsProvider', function (ChartJsProvider) {
     // Configure all charts
+    // Import colors into chart
     ChartJsProvider.setOptions({
-        chartColors: ['#c980ff', '#FF8A80', '#80b6ff'],
+        chartColors: ['#c980ff', '#FF8A80', '#80b6ff', "#008000"],
         responsive: true,
         beginAtZero: true
     });
 }]);
-streamApp.controller('MainController',['$scope', '$http', MainController]);
+streamApp.controller('MainController',['$scope', '$http', ChartController]);
 
-function MainController ($scope, $http) {
+function ChartController ($scope, $http) {
     // create a message to display in our view
     $scope.home = "Welcome Home!";
 
     // REQUEST OPTIONS USING GET METHOD.
-    var request = {
+    var request1 = {
         method: 'get',
         url: './data/platform.json',
         dataType: 'json',
         contentType: "application/json"
     };
 
-    $scope.arrSeries = new Array;
-    $scope.arrLabels = new Array;
-    $scope.arrData = new Array;
+    $scope.arrSeries1 = new Array;
+    $scope.arrLabels1 = new Array;
+    $scope.arrData1 = new Array;
 
-    $http(request)
+    $http(request1)
         .then(function onSuccess(jsonData) {
 
             // LOOP THROUGH DATA IN THE JSON FILE.
             angular.forEach(jsonData.data, function (item) {
                 //Get platform values in each object
-               $scope.arrSeries.push(item.platform);
+               $scope.arrSeries1.push(item.platform);
 
                 //Get all property names in each object
-               $scope.labels = Object.keys(item);
-               $scope.arrLabels = $scope.labels.slice(1,10);// Get property names between index[1] and index[10]
+               $scope.labels1 = Object.keys(item);
+               $scope.arrLabels1 = $scope.labels1.slice(1,10);// Get property names between index[1] and index[10]
 
                 //Get values in each object
-                $scope._data = jsonData.data.map(function(item) {
+                $scope._data1 = jsonData.data.map(function(item) {
                     return Object.keys(item)
                         .filter(function(key){
                             return key !=='live' && key !=='platform' //eliminate values of platform & live properties
@@ -48,15 +49,15 @@ function MainController ($scope, $http) {
                         return item[key]
                     });
                 });
-                $scope.arrData = $scope._data;
+                $scope.arrData1 = $scope._data1;
 
             });
 
-            $scope.series = $scope.arrSeries;
-            $scope.labels = $scope.arrLabels;
-            $scope.data = $scope.arrData;
+            $scope.series1 = $scope.arrSeries1;
+            $scope.labels1 = $scope.arrLabels1;
+            $scope.data1 = $scope.arrData1;
 
-            $scope.options = {
+            $scope.options1 = {
                 legend: { display: true },
                 title: {
                     display: true,
@@ -64,7 +65,61 @@ function MainController ($scope, $http) {
                 }
             };
         });
+
+
+    // REQUEST OPTIONS USING GET METHOD.
+    var request2 = {
+        method: 'get',
+        url: './data/country.json',
+        dataType: 'json',
+        contentType: "application/json"
+    };
+
+    $scope.arrSeries2 = new Array;
+    $scope.arrLabels2 = new Array;
+    $scope.arrData2 = new Array;
+
+    $http(request2)
+        .then(function onSuccess(jsonData) {
+
+            //LOOP THROUGH DATA IN THE JSON FILE.
+            angular.forEach(jsonData.data, function (item) {
+
+                //Get platform values in each object
+                $scope.seriestab = Object.keys(item);
+                $scope.arrSeries2 = $scope.seriestab.slice(0,4); // Get property names of cdn, p2p and total and percentage
+
+                //Get all property names in each object
+                $scope.arrLabels2.push(item.country);
+
+                //Get values in each object
+                $scope._data2 = jsonData.data.map(function(item) {
+                    return Object.keys(item)
+                        .filter(function(key){
+                            return key !=='country' //eliminate values of percentage & country properties
+                        })
+                        .map(function(key) {
+                            return item[key]
+                        });
+                });
+                $scope.arrData2 = $scope._data2;
+
+            });
+
+            $scope.series2 = $scope.arrSeries2;
+            $scope.labels2 = $scope.arrLabels2;
+            $scope.data2 = $scope.arrData2;
+
+            $scope.options2 = {
+                legend: { display: true },
+                title: {
+                    display: true,
+                    text: 'Streams distribution in % per most powerful countries'
+                }
+            };
+        });
 }
+
 
 
 
